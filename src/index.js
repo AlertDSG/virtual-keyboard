f7d437b8eefd5e30a193f26a00fd811cb5b49ef3
dima <AlertDSG@gmail.com>
2023-05-02 01:17:55 +0300

    refactor: space key and add descripe

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import data from './db.js';
 let lang = 'ru';
 let value;
 
-const arr = ['Backspace', 'ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'ContextMenu', 'ControlRight', 'ShiftLeft', 'Enter', 'CapsLock', 'ShiftRight'];
+const arr = ['Backspace', 'ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'ContextMenu', 'ControlRight', 'ShiftLeft', 'Enter', 'CapsLock', 'ShiftRight', 'Space'];
 
 function getLocal() {
   if (localStorage.getItem('data')) {
@@ -24,7 +24,7 @@ function logDownKey(e) {
   if (!document.querySelector(`.keyboard__key[data--Key=${e.code}]`)) {
     e.preventDefault();
     return;
-  };
+  }
 
   if (e.code === 'CapsLock') {
     const capsLock = document.querySelector('.caps-lock');
@@ -127,8 +127,12 @@ function init() {
     child.classList.add('keyboard__key');
     keyboard.appendChild(child);
   });
+
+  const describe = document.createElement('p');
+  describe.innerText = 'Клавиатура создана в операционной системе Windows \n Для переключения языка комбинация: lefts shift + alt';
   container.append(textField);
   container.append(keyboard);
+  container.append(describe);
   window.document.body.prepend(container);
   document.querySelector('.text-field').addEventListener('keydown', logDownKey);
   document.querySelector('.text-field').addEventListener('keyup', logUpKey);
@@ -140,6 +144,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 document.addEventListener('mousedown', (e) => {
   if (e.target.dataset.Key) {
+    const textField = document.querySelector('.text-field');
     if (e.target.dataset.Key === 'Tab') {
       document.querySelector('.text-field').value += '\t';
       document.querySelector(`.keyboard__key[data--Key=${e.target.dataset.Key}]`).classList.add('keyboard__key-active');
@@ -147,14 +152,16 @@ document.addEventListener('mousedown', (e) => {
       document.querySelector('.text-field').value += e.target.innerText;
       document.querySelector(`.keyboard__key[data--Key=${e.target.dataset.Key}]`).classList.add('keyboard__key-active');
     } else if (e.target.dataset.Key === 'Enter') {
-      document.querySelector('.text-field').value += '\n';
+      textField.value += '\n';
       document.querySelector('.enter').classList.remove('enter-passive');
       document.querySelector('.enter').classList.add('enter-active');
     } else if (e.target.dataset.Key === 'AltLeft') {
       document.querySelector(`.keyboard__key[data--Key=${e.target.dataset.Key}]`).classList.add('keyboard__key-active');
     } else if (e.target.dataset.Key === 'Backspace') {
-      const textField = document.querySelector('.text-field');
       textField.value = textField.value.slice(0, -1);
+      document.querySelector(`.keyboard__key[data--Key=${e.target.dataset.Key}]`).classList.add('keyboard__key-active');
+    } else if (e.target.dataset.Key === 'Space') {
+      textField.value += ' ';
       document.querySelector(`.keyboard__key[data--Key=${e.target.dataset.Key}]`).classList.add('keyboard__key-active');
     } else {
       logDownKey({ code: e.target.dataset.Key, key: e.target.innerText });
